adeacc50c317a6153dca01808e82c8fc609f1ef6
Nuthan <nuthan.s@comviva.com>
2024-05-24 08:24:01 +0530

    Updated the login and register page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,11 +3,12 @@ import { useNavigate, Link } from 'react-router-dom';
 import { Input, Button, message, Form, Card } from 'antd';
 import axios from 'axios';
 import './Login.css';
- 
+import newBackgroundImage from '../assets/login_2.svg'; 
+
 const Login = ({ onLogin }) => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
- 
+
   const onFinish = async (values) => {
     setLoading(true);
     try {
@@ -24,31 +25,62 @@ const Login = ({ onLogin }) => {
       setLoading(false);
     }
   };
- 
+
   return (
-    <div className="login-container">
-      <div className="background-overlay" />
-      <div className="login-box-container">
-        <Card className="login-box" hoverable>
-          <h3>LOGIN</h3>
-          <Form onFinish={onFinish} className="login-form">
-            <Form.Item name="identifier" rules={[{ required: true, message: 'Please input your username or email!' }]}>
-              <Input placeholder="Username or Email" />
-            </Form.Item>
-            <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
-              <Input.Password placeholder="Password" />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit" loading={loading} className="login-button">
-                Login
-              </Button>
-            </Form.Item>
-          </Form>
-          <p>Don't have an account? <Link to="/register">Register now!</Link></p>
-        </Card>
+    <div className="main-container">
+      <div className="main-logo-container">
+        <div className="comviva-main-logo"></div>
+      </div>
+      <div className="main-background-container"></div>
+      <div className="main-container-popup">
+        <div className="login-content-container">
+          <div className="login-popup-background-container">
+            <div className="login-mobilytix-logo">
+              MobiLytix <span className="trade-mark">™</span>
+            </div>
+            <div className="login-mobilytix-description">Rewards</div>
+            <div className="login-background-container">
+              <img src={newBackgroundImage} alt="Side" />
+            </div>
+          </div>
+          <div className="divider">
+            <div className="divider-line"></div>
+            <div className="divider-highlight"></div>
+            <div className="divider-line"></div>
+
+          </div>
+          <div className="login-popup-form-container">
+          <div className="top-right-logo"></div>
+
+            <Card className="login-box">
+              <h3>Welcome,</h3>
+              <p>Please login to get started</p>
+              <Form onFinish={onFinish} className="login-form">
+                <Form.Item
+                  name="identifier"
+                  rules={[{ required: true, message: 'Please input your username or email!' }]}
+                >
+                  <Input placeholder="Username or Email" className="textbox" />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                  <Input.Password placeholder="Password" className="textbox" />
+                </Form.Item>
+                <Form.Item>
+                  <Button type="primary" htmlType="submit" loading={loading} className="comviva-button">
+                    Sign In
+                  </Button>
+                </Form.Item>
+              </Form>
+              <p>Don't have an account? <Link to="/register">Register now!</Link></p>
+            </Card>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
- 
+
 export default Login;
